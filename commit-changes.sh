0ac3b049e9e687056a9b9fa9a3e91341bfbe53ff
Egor Koleda <radioegor146@yandex.ru>
2020-07-13 00:54:55 +0300

    add autocommit

---
+++ commit-changes.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+mkdir ~/production
+cd ~/production
+git clone https://github.com/GravitLauncher/WikiProduction
+cd WikiProduction
+rm -rf !("CNAME"|".git/")
+cp -r $GITHUB_WORKSPACE/dist/ ./
+git commit -m "Autoupdate from source repo"
+git push
