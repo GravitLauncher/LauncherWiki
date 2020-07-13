9a2bb69976b3e1f5ad7849a33290b4afe63a48ab
Egor Koleda <radioegor146@yandex.ru>
2020-07-13 08:44:36 +0300

    Update commit-changes.sh

--- commit-changes.sh
+++ commit-changes.sh
@@ -4,6 +4,8 @@ cd ~/production
 git clone https://github.com/GravitLauncher/WikiProduction
 cd WikiProduction
 rm -rf '(^"CNAME"|".git/")'
+echo "$GITHUB_WORKSPACE/dist/"
+ls -la $GITHUB_WORKSPACE/dist/
 cp -r $GITHUB_WORKSPACE/dist/ ./
 git config --global user.email "auto@gravit.pro"
 git config --global user.name "auto-commiter"
