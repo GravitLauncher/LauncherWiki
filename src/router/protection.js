53aaa078d19f124ee06f43d487727ede2bda2ce7
Gravita <gravita@gravit.pro>
2021-08-16 09:01:53 +0700

    [FIX] Dynamic title p.2

--- src/router/protection.js
+++ src/router/protection.js
@@ -2,21 +2,25 @@ export default [
   {
     path: '/protection/nginx',
     name: 'WikiProtectionNginx',
+    meta: { title: 'Защита | Настройка Nginx' },
     component: () => import('@/views/protection/Nginx')
   },
   {
     path: '/protection/protecthandler',
     name: 'WikiProtectionProtectHandler',
+    meta: { title: 'Защита | Настройка ProtectHandler' },
     component: () => import('@/views/protection/ProtectHandler')
   },
   {
     path: '/protection/signing',
     name: 'WikiProtectionSigning',
+    meta: { title: 'Защита | Подпись' },
     component: () => import('@/views/protection/Signing')
   },
   {
     path: '/protection/runtimeencrypt',
     name: 'WikiProtectionRuntimeEncrypt',
+    meta: { title: 'Защита | Шифрование рантайма' },
     component: () => import('@/views/protection/RuntimeEncrypt')
   }
 ]
